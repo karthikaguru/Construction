c0e209d04667e51349203a381d5de43fb7e7ceb8
karthikaguru <dglkarthika97@gmail.com>
2025-03-11 18:52:30 +0530

    deleting old file

--- projectsite/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import client_onboarding_view, project_dashboard_view, status_update_view
-
-urlpatterns = [
-    path('client-onboarding/', client_onboarding_view, name='client_onboarding'),
-    path('dashboard/', project_dashboard_view, name='project_dashboard'),
-    path('status-update/<int:project_id>/', status_update_view, name='status_update'),
-]
